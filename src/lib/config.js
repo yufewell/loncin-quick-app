7a7aa44046bc72412b9a09ba3fa43af0afff6a15
wangpeng <wangpeng@zcool.com.cn>
2019-07-28 09:53:56 +0800

    jiaofeixiangqing

--- src/lib/config.js
+++ src/lib/config.js
@@ -14,7 +14,39 @@ const noticeTabs = [
   },
 ]
 
+const payTypes = [
+  {
+    typeId: 0,
+    typeName: '全部记录',
+  },
+  {
+    typeId: 1,
+    typeName: '物业费',
+  },
+  {
+    typeId: 2,
+    typeName: '电费',
+  },
+  {
+    typeId: 3,
+    typeName: '水费',
+  },
+  {
+    typeId: 4,
+    typeName: '燃气费',
+  },
+  {
+    typeId: 5,
+    typeName: '停车费',
+  },
+  {
+    typeId: 2,
+    typeName: '供暖费',
+  },
+]
+
 module.exports = {
   datas,
   noticeTabs,
+  payTypes,
 }
